ef1ea4b6bed3ba20a5eda0be58e98c4f3865a40e
fitplusSTRONGGYM <119040146+fitplusSTRONGGYM@users.noreply.github.com>
2022-11-25 02:06:09 -0700

    Add files via upload

---
+++ alimentacion.php
@@ -0,0 +1,175 @@
+<!DOCTYPE HTML>
+<!--
+	Alpha by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>FitPlus+ | Alimentación</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="icon" type="image" href="images/icono3.jpg">
+	</head>
+	<body class="is-preload">
+		<div id="page-wrapper">
+<!-- Header -->
+				<header id="header">
+	
+					
+					<div class="logo">
+						<a href="menu_principal.html"><img src="logo3.jpg" width="210" height="50" alt="For Strong Gym" class="logo-img"></a>
+					
+					</div>
+
+					<nav id="nav">
+						<ul>
+							
+							<li>
+								<a href="alimentacion.html" class="icon solid fa-angle-down">Nutrición</a>
+								<ul>
+									<li><a href="">directorio</a></li>
+								</ul>
+							</li>
+		
+							<li><a href="ejercicios.html" class="icon solid fa-angle-down">Ejercicio</a>
+								<ul>
+									<li><a href="">Registro</a></li>
+									<li><a href="">Calculadora</a></li>
+									<li><a href="">Ejercicios</a></li>
+								</ul>
+							</li>	
+							
+							<li><a href="#" class="icon solid fa-angle-down">Enfermedades</a></li>
+
+						</ul>
+					</nav>
+				</header>
+
+			<!-- Main -->
+				<section id="main" class="container">
+					<header>
+						<h2>Una Alimentación Sana</h2>
+						<p>Tener una alimentación saludable ayuda a tener una buena calidad de vida</p>
+					</header>
+					<div class="box">
+						<p><img alt="Cambiar imagen" class="image featured" onmouseout="this.src='images/al5.jpg';" onmouseover="this.src='images/al3.jpg';" src="images/al5.jpg" /></p><br><br>
+						<h3>Importancia de una alimentación saludable</h3>
+						<p>La alimentación es una actividad fundamental en nuestra vida. Por una parte,tiene un papel importantísimo en la socialización del ser humano y su relación con el medio que le rodea. Por otra, es el proceso por el cual obtenemos los nutrientes que nuestro cuerpo necesita para vivir.<br>De igual manera es de suma importancia mantener una alimentacion sana y equilibrada para tener una mejor calidad de vida.</p>
+
+						<div class="row">					
+								<div class="row-6 row-12-mobilep">
+								<h3>En que esta basada una alimentanción saludable</h3>
+
+								<p> Una alimentación saludable mas que nada esta basada en ingerir alimentos que le aporten al cuerpo los nutrientes y la energia necesaria que este necesita, para poder vivir.<br>La base de una alimentacion saludable se inicia cuando adquirimos la conciencia de lo que implica para nuestro cuerpo. de los alimentosobtenemos la energia y los nutrimentos ue permiten que cada parte de nuestro organismo cumplacon sus funciones.<br>se denomina "dieta" al conjunto de alimentos y platillos que se consumen cada dia para que la dieta sea buena, debe tener ciertas caracteristicas: Ser completa, equilibrada, inocua, suficiente, variada y adecuada.</p>
+
+								<img alt="Cambiar imagen" height="198" onclick="this.src='images/al8.jpg';" onmouseover="this.src='images/al6.jpg';"src="images/al8.jpg">
+								<p>Una dieta <span>Completa, </span>se refiere a la cantidad de nutrimentro que contenga. Cada persona, de acuerdo a su edad, genero, composicion corporal y actividad fisica, necesita un cierta cantidad de ellos para que su organismofuncione adecuadamente;<span>equilibrada, </span>se basa en que combine los alimentos correctamente.; <span>inocua, </span> es decir, libre de microorganismos, toxinas y agentes contaminantes que pueden causar enfermedades; <span>suficiente, </span>para que asi aporte la energia adecuada que permieta mantener un peso corporal saludable u en cada una de las estapas de la vida.</p>
+							<div class="wrap">
+								<h3>En que se basa una alimentacion saludable</h3>
+<div class="table">
+<ul>
+  
+<li>
+  <div class="top">
+    <h1>Entrada<br>Sopa de dos colores</h1>
+    <div class="circle">$100</div>
+  </div>
+  <div class="bottom">
+    <p><span>1.-</span>1 taza de caldo de pollo<br><span>2.-</span>1 latta de hutlacoche<br><span>3.-</span>4 papas<br><span>4.-</span>2 tazas de granos de elote<br><span>5.-</span>3 taza de leche entera<br><span>6.-</span>Sal al gusto<br></p>
+    <p><span>PROCEDIMIENTO</span></p>
+    <p>Cocer la papa, formar un puré y disolver con la leche y el caldo. Hervir por 10 min. Dividirla en dos partes iguales y agregar a una el huitlacoche y a la otra el elote licuado, una vez mas poner al fuego 10 min. Rectificar el sazón y servir en tazon al mismo tiempo para que no se mezclen los sabores y colores.</p>
+  </div>
+</li>
+  
+<li>
+  <div class="top">
+    <h1>Plato fuerte<br>Chiles rellenos de soya</h1>
+    <div class="circle">$110</div>
+  </div>
+    <div class="bottom">
+    <p><span>1.-</span> 6 chiles poblanos asados y desvenados<br><span>2.-</span> 4 tazas de soya deshidratada<br><span>3.-</span> 4 tomates<br><span>4.-</span>1 cebolla chica<br><span>5.-</span> 1 diente de ajo<br><span>6.-</span> 4 rabos de cebollitas cambray picados<br><span>7.-</span> 1 manojo pequeño de cilantro<br><span>8.-</span> 1 taza de crema<br><span>9.-</span> 3 cdas. de aceite de oliva</p>
+    <p><span>PROCEDIMIENTO</span></p>
+    <p>Escurrir la soya hasta quedar seca o sin liquido. Guisarla con el tomate, la cebolla y ajo. Cocinar por 7 minutos, agrgar cilantro picado y el rabo de las cebollitas. Rellenar los chiles. Servir agregando crema sobre el relleno.</p>
+  </div>
+</li>
+  
+<li>
+  <div class="top">
+    <h1>Postre<br>Gelatina de naranjas con fresas</h1>
+    <div class="circle">$70</div>
+  </div>
+    <div class="bottom">
+    <p><span>1.-</span> 2 paquetes de gelatina de naranja baja en calorías<br>
+    <span>2.-</span> 3 tazas de fresas<br>
+    <span>3.-</span> 6 tazas de agua</p>
+    <p><span>PROCEDIMIENTO</p>
+    <p>Disolver la gelatina siguiendo las instrucciones del paquete.<br> Verter a moldes y refrigerar hasta tomar la consisitencia deseada.<br> Retirar del molde y decorar con las fresas.</p><br>
+  </div>
+</li>
+</ul>
+<ul>
+<li>
+  <div class="top">
+    <h1>Postre<br>Fruta con yogur orgánico</h1>
+    <div class="circle">$150</div>
+  </div>
+    <div class="bottom">
+    <p><span>1.-</span> 4 tazas de yogur orgánico<br><span>2.-</span> 3 peras<br><span>3.-</span> 3 manzanas<br><span>4.-</span> 6 kiwis<br><span>5.-</span> 1 taza de tapioca cocida<br></p>
+    <p><span>PROCEDIMIENTO</span></p>
+    <p>Cortar las frutas en cubos pequeños. Servir el yogur en proporciones iguales agregandole la fruta, finalizando con la tapioca y decora al gusto.</p>
+  </div>
+</li>
+<li>
+  <div class="top">
+    <h1>Entrada<br>Aros de calabaza</h1>
+    <div class="circle">$100</div>
+  </div>
+  <div class="bottom">
+    <p><span>1.-</span> 6 calabazas de arbol <span>2.-</span> 3 tazas de arroz cocido para sushi<br><span>3.-</span> 2 tazas de requesón<br><span>4.-</span> 8 tomatillo verdes<br><span>5.-</span> 1 manojo pequeño de cilantro<br><span>6.-</span> 2 chiles cuaresmeños<br><span>7.-</span> 2 dientes de ajo<br><span>8.-</span> 1/4 de cebolla blanca<br><span>9.-</span> Sal al gusto<br></p>
+    <p><b>PROCEDIMIENTO</b></p>
+    <p>Ponchar las calabazas quitandoles las pepitas. Asar los tomatillos, el chile y la ceboola sin dejar que esta ultima se obscurezca. Cocer con muy poca agua y licuar agregando el cilantro y el ajo. Sazonar al gusto. Rellenar las calabazas con el requeson y rebanarlas diagonalmente. Servir las rodajas de calabaza sobre una cama de salsa y acompañar con una porcion de arroz.</p>
+  </div>
+</li>
+<li>
+  <div class="top">
+    <h1>Plato fuerte<br>Enfrijoladas</h1>
+    <div class="circle">$129</div>
+  </div>
+    <div class="bottom">
+    <p><span>1.-</span> 8 tortillas de maiz<br><span>2.-</span> 1/2 taza de frijoles molidos aguados<br><span>3.-</span> 4 cucharadas de sustituto de crema agria<br><span>4.-</span> 1 receta de tofu revuelto<br><span>5.-</span> 1 aguacate <br></p>
+    <p><span>PROCEDIMIENTO</span></p>
+    <p>Caliento las tortillas de maiz y sumerjelas una por una en los frijoles. Rellena cada tortilla con una porcion de tofu revuelto y haz taquitos. Baña con una cucharada de crema cada platillo y decora con rebanada de aguacate.</p>
+  </div>
+</li>
+
+
+
+  
+</ul>
+</div>
+</div>
+
+						</div>
+			</section>
+
+						<!-- Footer -->
+				<footer id="footer">
+					<ul class="copyright">
+						<li>&copy; Un proyecto de FitPlus+, para Strong Gym.</li>
+					</ul>
+				</footer>
+		</div></div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>FITPLUS+ | Registro</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="landing is-preload">
+		<div id="page-wrapper">
+
+			<!-- Banner -->
+				<section id="banner">
+					<h2>Fit Plus +</h2>
+					<p>Registrate o inicia sesión para ver más información</p>
+					<ul class="actions special">
+						<li><a href="#registro" class="button primary">Registrarse</a></li>
+						<li><a href="#IniciarSesion" class="button">Iniciar sesión</a></li>
+					</ul>
+				</section>
+
+			<!-- Main -->
+				<section id="main" class="container">
+					<section id="main" class="container medium">
+						
+						<br><br><br><br><br><br><br><a name="IniciarSesion"></a><br><br><br>
+						<div class="box">
+							<header>
+								<h2>Iniciar sesión</h2>
+							</header>
+							<form method="post" action="#">
+								<div class="row gtr-50 gtr-uniform">
+									
+									<div class="col-12">
+										<input type="text" name="email" id="correo" value="" placeholder="Correo Electrónico" />
+									</div>
+									<div class="col-12">
+										<input type="password" name="subject" id="subject" value="" placeholder="Contraseña" />
+									</div>
+									
+									<div class="col-12">
+										<ul class="actions special">
+											<li><input type="submit" value="Iniciar sesión" /></li>
+										</ul>
+									</div>
+								</div>
+							</form>
+						</div>
+					</section>
+					<br><br><br>
+				<section id="main" class="container">
+					<section id="main" class="container medium">
+
+						<br><br><br><br><br><br><a name="registro"></a><br><br><br>
+						<div class="box">
+							<header>
+								<h2>Regístrate</h2>
+								<p>Llena los campos disponibles</p>
+							</header>
+							<form method="post" action="#">
+								<div class="row gtr-50 gtr-uniform">
+									<div class="col-6 col-12-mobilep">
+										<input type="text" name="name" id="name" value="" placeholder="Nombre(s)" />
+									</div>
+									<div class="col-6 col-12-mobilep">
+										<input type="text" name="apellidos" id="apellidos" value="" placeholder="Apellidos" />
+									</div><div class="col-6 col-12-mobilep">
+										<input type="text" name="name" id="name" value="" placeholder="Edad" />
+									</div>
+									<div class="col-6 col-12-mobilep">
+										<input type="text" name="sexo" id="apellidos" value="" placeholder="Sexo" />
+									</div>
+									<div class="col-12">
+										<input type="email" name="email" id="correo" value="" placeholder="Correo Electrónico" />
+									</div>
+									<div class="col-12">
+										<input type="password" name="subject" id="subject" value="" placeholder="Contraseña" />
+									</div>
+									
+									<div class="col-12">
+										<ul class="actions special">
+											<li><input type="submit" value="Regístrarte" /></li>
+										</ul>
+									</div>
+								</div>
+							</form>
+						</div>
+					</section>
+				
+
+						<!-- Footer -->
+				<footer id="footer">
+					<ul class="copyright">
+						<li>&copy; Un proyecto de FitPlus+, para Strong Gym.</li>
+					</ul>
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
